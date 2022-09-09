7eaf995643bd5d20a5e3b3f02213eacacc4ec15c
Wang-Zhongwei <873994270@qq.com>
2022-09-09 11:11:26 -0400

    done

--- myApp.js
+++ myApp.js
@@ -25,48 +25,108 @@ const createAndSavePerson = (done) => {
   });
 };
 
+// create an array of people by using create() method
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, function (err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
+/**
+ * findEditThenSave is a pipeline:
+ * Env --personId--> [findById] --person--> [save] --updatedPerson--> [done] --> Env
+ * @param {Number} personId
+ * @param {Function} done a callback function that is called after the current function has finished
+ */
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
+  // Parameters of callback function are (err, data) => {}:
+  //    err represents any error that occurred during the previous operation.
+  //    data represents the data returned from the previous operation.
+  Person.findById({ _id: personId }, (err, person) => {
+    if (err) return console.log(err);
 
-  done(null /*, data*/);
+    person.favoriteFoods.push(foodToAdd);
+
+    person.save((err, updatedPerson) => {
+      if (err) return console.log(err);
+      done(null, updatedPerson);
+    });
+  });
 };
 
+/**
+ *
+ * @param {String} personName
+ * @param {Function} done
+ */
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  // findOneAndUpdate() has a callback function parameter that takes 2 parameters: err and updatedDoc
+  // err represents any error that occurred during the previous operation.
+  // updatedDoc is the document that was updated
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (err, updatedDoc) => {
+      if (err) return console.log(err);
+      done(null, updatedDoc);
+    }
+  );
 };
 
-const removeById = (personId, done) => {
-  done(null /*, data*/);
+var removeById = function (personId, done) {
+  // removedDoc is a json that contains the info of the removed person
+  Person.findByIdAndRemove(personId, (err, removedDoc) => {
+    if (err) return console.log(err);
+    done(null, removedDoc);
+  });
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  // outcome is a json object that contains the number of documents that were removed
+  Person.remove({ name: nameToRemove }, (err, outcome) => {
+    if (err) return console.log(err);
+    done(null, outcome);
+  });
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ age: 0 })
+    .exec((err, data) => {
+      if (err) return console.log(err);
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
